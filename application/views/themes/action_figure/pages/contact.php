9e928fbea781b027f6ff2f476e88be6aa2931b1c
rickysoe <rickysoedirman25@gmail.com>
2022-12-08 19:50:49 +0700

    first commit

---
+++ application/views/themes/action_figure/pages/contact.php
@@ -0,0 +1,79 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<div class="hero-wrap hero-bread" style="background-image: url('<?php echo get_theme_uri('images/bg_1.jpg'); ?>');">
+    <div class="container">
+      <div class="row no-gutters slider-text align-items-center justify-content-center">
+        <div class="col-md-9 ftco-animate text-center">
+            <p class="breadcrumbs"><span class="mr-2"><?php echo anchor(base_url(), 'Home'); ?></span> <span>Hubungi Kami</span></p>
+          <h1 class="mb-0 bread">Hubungi Kami</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <section class="ftco-section contact-section bg-light">
+    <div class="container">
+        <div class="row d-flex mb-5 contact-info">
+        <div class="w-100"></div>
+        <div class="col-md-3 d-flex">
+            <div class="info bg-white p-4">
+              <p><span>Alamat:</span> <br><?php echo get_settings('store_address'); ?></p>
+            </div>
+        </div>
+        <div class="col-md-3 d-flex">
+            <div class="info bg-white p-4">
+              <p><span>No. HP:</span> <br> <?php echo get_settings('store_phone_number'); ?></p>
+            </div>
+        </div>
+        <div class="col-md-3 d-flex">
+            <div class="info bg-white p-4">
+              <p><span>Email:</span> <br> <?php echo get_settings('store_email'); ?></p>
+            </div>
+        </div>
+        <div class="col-md-3 d-flex">
+            <div class="info bg-white p-4">
+              <p><span>Website:</span> <br> www.sugoi-shop.com</p>
+            </div>
+        </div>
+      </div>
+      <div class="row block-9">
+        <div class="col-md-6 order-md-last d-flex">
+          <form action="<?php echo site_url('pages/send_message'); ?>" class="bg-white p-5 contact-form" method="POST">
+          <?php if ($flash) : ?>
+          <div class="text-success text-center" style="margin-bottom: 25px;"><?php echo $flash; ?></div>
+          <?php endif; ?>
+
+            <div class="form-group">
+              <input type="text" name="name" class="form-control" value="<?php echo set_value('name', (is_login() ? get_user_name() : '')); ?>" placeholder="Nama" required>
+              <?php echo form_error('name'); ?>
+            </div>
+            <div class="form-group">
+              <input type="email" name="email" class="form-control" value="<?php echo set_value('email', (is_login() ? $user->email : '')); ?>" placeholder="Email" required>
+              <?php echo form_error('email'); ?>
+            </div>
+            <div class="form-group">
+              <input type="text" name="subject" class="form-control" value="<?php echo set_value('subject'); ?>" placeholder="Subjek pesan" required>
+              <?php echo form_error('subject'); ?>
+            </div>
+            <div class="form-group">
+              <textarea name="message" id="" cols="30" rows="7" class="form-control" placeholder="Pesan" required><?php echo set_value('message'); ?></textarea>
+              <?php echo form_error('message'); ?>
+            </div>
+            <div class="form-group">
+              <input type="submit" value="Kirim Pesan" class="btn btn-primary py-3 px-5">
+            </div>
+          </form>
+        
+        </div>
+
+        <div class="col-md-6 d-flex">
+            <div style="width: 100%">
+                <iframe width="100%" height="600" src= "https://www.google.co.id/maps/place/Jl.+Raya+Kb.+Jeruk+No.99,+RT.2%2FRW.9,+Kb.+Jeruk,+Kec.+Kb.+Jeruk,+Kota+Jakarta+Barat,+Daerah+Khusus+Ibukota+Jakarta+11530/@-6.1990307,106.7804317,17z/data=!3m1!4b1!4m5!3m4!1s0x2e69f6dd7194e1bb:0x30711eecf3cc6f4b!8m2!3d-6.1990307!4d106.7804317" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+                    <a href="https://www.maps.ie/coordinates.html">find my coordinates</a>
+                </iframe>
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
